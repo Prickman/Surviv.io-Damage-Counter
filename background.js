277ad88e806c3703c6034df8152a428d289fc35b
Allan Cao <29407186+Allan-Cao@users.noreply.github.com>
2018-05-15 11:13:31 -0600

    remove

--- background.js
+++
@@ -1,7 +0,0 @@
-// background.js
-
-// Called when the user clicks on the extension
-chrome.browserAction.onClicked.addListener(function(tab) {
-  // open surviv.io in a new tab
-  chrome.tabs.create({"url": "http://surviv.io"});
-});
